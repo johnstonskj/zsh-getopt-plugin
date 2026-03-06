543db79b6376031bf24bb155a2941dc4556b6072
Simon Johnston <johnstonskj@gmail.com>
2026-03-06 07:40:47 -0800

    feeat: using new zplugins format

--- getopt.plugin.zsh
+++ getopt.plugin.zsh
@@ -1,97 +1,12 @@
 # -*- mode: sh; eval: (sh-set-shell "zsh") -*-
 #
-# Plugin Name: getopt
-# Description: Zsh plugin to set the correct path for get-opt installed via Homebrew.
-# Repository: https://github.com/johnstonskj/zsh-getopt-plugin
-#
-# Public variables:
-#
-# * `GETOPT`; plugin-defined global associative array with the following keys:
-#   * `_FUNCTIONS`; a list of all functions defined by the plugin.
-#   * `_PLUGIN_DIR`; the directory the plugin is sourced from.
-#   * `_PATH`; path to the gnu-getopt binary.
+# @name getopt
+# @brief Zsh plugin to set the correct path for get-opt installed via Homebrew.
+# @repository https://github.com/johnstonskj/zsh-getopt-plugin
 #
 
-############################################################################
-# Standard Setup Behavior
-############################################################################
-
-# See https://wiki.zshell.dev/community/zsh_plugin_standard#zero-handling
-0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
-0="${${(M)0:#/*}:-$PWD/$0}"
-
-# See https://wiki.zshell.dev/community/zsh_plugin_standard#standard-plugins-hash
-declare -gA GETOPT
-GETOPT[_PLUGIN_DIR]="${0:h}"
-GETOPT[_FUNCTIONS]=""
-
-# Set the path for any custom directories here.
-GETOPT[_PATH]="$(homebrew_formula_prefix gnu-getopt)/bin"
-
-############################################################################
-# Internal Support Functions
-############################################################################
-
-#
-# This function will add to the `GETOPT[_FUNCTIONS]` list which is
-# used at unload time to `unfunction` plugin-defined functions.
-#
-# See https://wiki.zshell.dev/community/zsh_plugin_standard#unload-function
-# See https://wiki.zshell.dev/community/zsh_plugin_standard#the-proposed-function-name-prefixes
-#
-.getopt_remember_fn() {
-    builtin emulate -L zsh
-
-    local fn_name="${1}"
-    if [[ -z "${GETOPT[_FUNCTIONS]}" ]]; then
-        GETOPT[_FUNCTIONS]="${fn_name}"
-    elif [[ ",${GETOPT[_FUNCTIONS]}," != *",${fn_name},"* ]]; then
-        GETOPT[_FUNCTIONS]="${GETOPT[_FUNCTIONS]},${fn_name}"
-    fi
-}
-.getopt_remember_fn .getopt_remember_fn
-
-#
-# This function does the initialization of variables in the global variable
-# `GETOPT`. It also adds to `path` and `fpath` as necessary.
-#
 getopt_plugin_init() {
     builtin emulate -L zsh
-    builtin setopt extended_glob warn_create_global typeset_silent no_short_loops rc_quotes no_auto_pushd
 
-    path+=( "${GETOPT[_PATH]}" )
+    @zplugins_add_to_path getopt "$(homebrew_formula_prefix gnu-getopt)/bin"
 }
-.getopt_remember_fn getopt_plugin_init
-
-############################################################################
-# Plugin Unload Function
-############################################################################
-
-# See https://wiki.zshell.dev/community/zsh_plugin_standard#unload-function
-getopt_plugin_unload() {
-    builtin emulate -L zsh
-
-    # Remove all remembered functions.
-    local plugin_fns
-    IFS=',' read -r -A plugin_fns <<< "${GETOPT[_FUNCTIONS]}"
-    local fn
-    for fn in ${plugin_fns[@]}; do
-        whence -w "${fn}" &> /dev/null && unfunction "${fn}"
-    done
-
-    path=( "${(@)path:#${GETOPT[_PATH]}}" )
-    
-    # Remove the global data variable.
-    unset GETOPT
-
-    # Remove this function.
-    unfunction getopt_plugin_unload
-}
-
-############################################################################
-# Initialize Plugin
-############################################################################
-
-getopt_plugin_init
-
-true
